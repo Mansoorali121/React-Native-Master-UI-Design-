8d7e462217765bf4309962992891b86be7a0b285
Mansoor Sahito <mansoorsahito601@gmail.com>
2025-08-04 10:05:20 +0500

    Home header add

--- src/screens/HomeScreen.jsx
+++ src/screens/HomeScreen.jsx
@@ -1,14 +1,32 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { s, vs } from 'react-native-size-matters';
 
 const HomeScreen = () => {
   return (
-    <View>
-      <Text>HomeScreen</Text>
+    <View
+      style={{
+        paddingTop: vs(50),
+        paddingHorizontal: s(16),
+      }}
+    >
+      <Text
+        style={{
+          color: '#1D150F',
+          fontSize: s(20),
+          fontWeight: 'semibold',
+          marginBottom: vs(6),
+        }}
+      >
+        Meditations
+      </Text>
+      <Text style={{ color: '#2C2016', fontSize: s(14), marginBottom:vs(16) }}>
+        Lorem Ipsum simply dummy text
+      </Text>
     </View>
-  )
-}
+  );
+};
 
 export default HomeScreen;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
